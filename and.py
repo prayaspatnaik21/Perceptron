a40ddd37fbbbe28c0ef23b7d724815ffee7294af
Prayas <prayaspatnaik21@gmail.com>
2022-01-19 12:49:47 -0600

    Adding Main function

--- and.py
+++ and.py
@@ -4,26 +4,31 @@ from utils.all_utils import prepare_data,save_plot,save_model
 import pandas as pd
 import numpy as np
 
+def main(data,eta,epochs,filename,plotFilename):
 
-AND = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,0,0,1],
-}
+    df = pd.DataFrame(data)
 
-df = pd.DataFrame(AND)
+    print(df)
 
-print(df)
+    X,y = prepare_data(df)
 
-X,y = prepare_data(df)
+    model = Perceptron(eta=eta,epochs=epochs)
+    model.fit(X, y)
 
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
+    _ = model.total_loss()
 
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
+    save_model(model,filename=filename)
+    save_plot(df,plotFilename,model)
 
-_ = model.total_loss()
+if __name__ == '__main__': # << entry point
 
-save_model(model,filename="and.model")
-save_plot(df,"and.png",model)
+    AND = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,0,0,1],
+    }
+
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+
+    main(data=AND,eta=ETA,epochs=EPOCHS,filename="and.model",plotFilename="and.png")
--- or.py
+++ or.py
@@ -4,26 +4,31 @@ from utils.all_utils import prepare_data,save_plot,save_model
 import pandas as pd
 import numpy as np
 
+def main(data,eta,epochs,filename,plotFilename):
 
-OR = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,1,1,1],
-}
+    df = pd.DataFrame(data)
 
-df = pd.DataFrame(OR)
+    print(df)
 
-print(df)
+    X,y = prepare_data(df)
 
-X,y = prepare_data(df)
+    model = Perceptron(eta=eta,epochs=epochs)
+    model.fit(X, y)
 
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
+    _ = model.total_loss()
 
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
+    save_model(model,filename=filename)
+    save_plot(df,plotFilename,model)
 
-_ = model.total_loss()
+if __name__ == '__main__': # << entry point
 
-save_model(model,filename="or.model")
-save_plot(df,"or.png",model)
+    OR = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,1,1,1],
+    }
+
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+
+    main(data=OR,eta=ETA,epochs=EPOCHS,filename="or.model",plotFilename="or.png")
